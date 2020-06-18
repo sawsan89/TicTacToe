07672f632436c1edfee3d910514cbfdb7cc1a1cb
sawsan89 <sawsan.h.m.89@gmail.com>
2020-06-18 16:35:42 +0300

    in_proccess

---
+++ app.js
@@ -0,0 +1,26 @@
+//2 players 
+//start always x
+//detect a win or tie and display >> can make it alert msg
+
+var player1;
+var player2;
+
+var table = document.getElementById("table");
+var cell0 = document.getElementById("table").rows[0].cells[0];
+//document.write(cell[0].innerHTML);
+
+var rows = document.getElementById("table").rows;
+
+
+
+function restart() {
+   // document.getElementById("table").reset();  
+   for (var i = 0; i < 3; i++) {
+    for (var j = 0; j <3; j++) {
+        rows[i].cells[j] = " ";
+        console.log(rows[i].cells[j])
+    }
+}
+  }
+
+// var row3 = document.getElementById("table").rows[2].cells.length;
